5b37e1d3bc87ef333c15ac4475ade96d77ea3dfc
lali <lali@riu.com>
2022-11-15 21:27:45 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { array } from './questions';
+import { Question, questions } from './questions';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +7,28 @@ import { array } from './questions';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
- @Input() answers!: string;
-  // public answers!: string
-  public time!: Number;
-  public bdc = array;
-  private wpm = 225;
+  @Input() answers!: string;
 
   ngOnInit(): void {
-    this.readingTime();
-    console.log(this.answers);
+    this.calculateAllAnswersReadingTime(questions);
   }
 
-  public readingTime() {
-    // const words = this.answers.trim().split(/\s+/).length;
-    // this.time = Math.ceil(words / this.wpm);
-  }
+  private calculateAllAnswersReadingTime = (questions: Question[]): any => {
+    const answers = this.getAnswers(questions);
+    console.log(this.getAllAnswersReadingTime(answers));
+  };
+
+  private getAllAnswersReadingTime = (answers: string[] | null) => {
+    return answers?.map(this.calculateReadingTime);
+  };
+
+  private calculateReadingTime = (answer: string): number => {
+    const wordsPerMinute = 225;
+    const wordsInAnswer = answer.trim().split(/\s+/).length;
+    const estimatedReadingTime = Math.ceil(wordsInAnswer / wordsPerMinute);
+    return estimatedReadingTime;
+  };
+
+  private getAnswers = (questions: Question[]): string[] | null =>
+    questions.map((question) => question.answer);
 }
--- src/app/questions.ts
+++ src/app/questions.ts
@@ -1,31 +1,38 @@
-export const array = [
+export interface Question {
+  question: string;
+  answer: string;
+}
+
+export const questions: Question[] = [
   {
     question: '¿Cómo se llama el perro que siempre acompaña a Obélix?',
-    answers:
+    answer:
       'Se llama Ideafix, o Idéfix en francés, el idioma original del cómic. (Las razas de perro con más fuerza de mordida del mundo)',
   },
   {
     question: '¿Quién escribió Territorio Comanche?',
-    answers:
+    answer:
       'Fue Arturo Pérez-Reverte, y fue publicado en 1994. (Los mejores libros de Arturo Pérez Reverte: ordenamos sus novelas)',
   },
   {
     question: '¿Cuántos cuentos tiene el Decamerón de Giovanni Bocaccio?',
-    answers:
+    answer:
       'El título de este clásico engaña. Son diez (deca) días (hemera), en los que cada uno de los diez jóvenes narra un cuento. 10*10= 100 cuentos que tiene el Decamerón, el libro favorito sobre cómo disfrutar la vida con elegancia en una pandemia del hombre Esquire del Quattrocento.',
   },
   {
     question:
       '¿Los cigarrillos electrónicos son menos dañinos que los cigarrillos clásicos?',
-    answers:
+    answer:
       'Tanto los productos de tabaco como los SEAN plantean riesgos para la salud. El enfoque más seguro es no consumir ni usar ninguno de ellos. Es probable que los niveles de riesgo asociados con el uso de SEAN o productos de tabaco dependan de una serie de factores, algunos de ellos relacionados con los productos utilizados y otros con el usuario individual. Esos factores incluyen el tipo y las características del producto, cómo se utilizan los productos, incluida la frecuencia de uso, cómo se fabrican los productos, quién está utilizando el producto y si las características del producto se manipulan después de la venta.La toxicidad no es el único factor a la hora de evaluar el riesgo de la exposición a las emisiones de los SEAN para una persona o población. Esos factores pueden incluir las posibilidades de abuso o manipulación del producto, el uso por parte de niños y adolescentes que de otro modo no habrían usado cigarrillos, el uso simultáneo con otros productos de tabaco (uso combinado o policonsumo), y los niños y adolescentes que siguen consumiendo productos de tabaco después de la experimentación con SEAN. Además, no todos los SEAN son iguales, y los riesgos para la salud pueden diferir de un producto a otro, y de un usuario a otro.',
   },
   {
     question: '¿Es peligroso viajar a lugares donde el paludismo es endérmico?',
-    answers: 'Las personas que no tienen inmunidad parcial al paludismo tienen un riesgo mayor de contraer la enfermedad. Ello incluye a los viajeros de países no endémicos que ingresan en zonas de alta transmisión, así como a las personas de países endémicos que viven en zonas donde hay poca o ninguna transmisión.Dado que a menudo los síntomas no se presentan hasta 10 o 15 días después de la infección, puede que los viajeros regresen a su país de origen sin mostrar signos de la enfermedad. Es posible que los médicos de las zonas no endémicas no reconozcan los síntomas y se produzcan retrasos potencialmente mortales del diagnóstico y el tratamiento. Además, puede que no en todos los países se hayan registrado medicamentos antipalúdicos eficaces o se disponga de ellos.Como tratamiento preventivo antes de viajar a zonas endémicas se puede utilizar la quimioprofilaxis. Cuando se combina con el uso de mosquiteros tratados con insecticida y la aplicación repetida de un repelente tópico para prevenir las picaduras de mosquitos, la quimioprofilaxis reduce significativamente el riesgo de infección. Si una persona ha recibido quimioprofilaxis como medida preventiva y no obstante es infectada, en su tratamiento no se debe utilizar el mismo medicamento.Se recomienda a los viajeros que consulten a un médico o a su centro nacional de control de enfermedades antes de la salida, para determinar las medidas preventivas adecuadas.',
+    answer:
+      'Las personas que no tienen inmunidad parcial al paludismo tienen un riesgo mayor de contraer la enfermedad. Ello incluye a los viajeros de países no endémicos que ingresan en zonas de alta transmisión, así como a las personas de países endémicos que viven en zonas donde hay poca o ninguna transmisión.Dado que a menudo los síntomas no se presentan hasta 10 o 15 días después de la infección, puede que los viajeros regresen a su país de origen sin mostrar signos de la enfermedad. Es posible que los médicos de las zonas no endémicas no reconozcan los síntomas y se produzcan retrasos potencialmente mortales del diagnóstico y el tratamiento. Además, puede que no en todos los países se hayan registrado medicamentos antipalúdicos eficaces o se disponga de ellos.Como tratamiento preventivo antes de viajar a zonas endémicas se puede utilizar la quimioprofilaxis. Cuando se combina con el uso de mosquiteros tratados con insecticida y la aplicación repetida de un repelente tópico para prevenir las picaduras de mosquitos, la quimioprofilaxis reduce significativamente el riesgo de infección. Si una persona ha recibido quimioprofilaxis como medida preventiva y no obstante es infectada, en su tratamiento no se debe utilizar el mismo medicamento.Se recomienda a los viajeros que consulten a un médico o a su centro nacional de control de enfermedades antes de la salida, para determinar las medidas preventivas adecuadas.',
   },
   {
     question: '¿Se puede eliminar paludismo?',
-    answers: 'La visión de la OMS y de la comunidad mundial interesada en esta enfermedad es un mundo sin paludismo. Esta visión se logrará progresivamente a medida que los países eliminen el paludismo de sus territorios y apliquen medidas eficaces para prevenir el restablecimiento de la transmisión.Los países donde el paludismo es endémico se encuentran en diferentes fases del camino que lleva a la eliminación. La tasa de progreso depende de la solidez del sistema nacional de salud, el nivel de inversión en estrategias de eliminación del paludismo y otros factores, incluidos los determinantes biológicos, el medio ambiente y las realidades sociales, demográficas, políticas y económicas de cada país en particular.En los últimos dos decenios se han logrado avances significativos hacia la eliminación del paludismo. Según el último Informe mundial sobre el paludismo, 26 países registraron menos de 100 casos de la enfermedad en 2020, frente a 6 países en 2000.Los países donde durante por lo menos tres años consecutivos no se ha registrado ningún caso autóctono de paludismo (un caso contraído localmente sin datos de importación de otro país endémico) pueden solicitar a la OMS la certificación de la eliminación del paludismo. Once países han recibido la certificación desde el año 2000, siendo China y El Salvador los que la han obtenido más recientemente. Desde 2015, nueve países han recibido la certificación del Director General de la OMS de que están exentos de paludismo, a saber, Maldivas (2015), Sri Lanka (2016), Kirguistán (2016), el Paraguay (2018), Uzbekistán (2018), la Argentina (2019), Argelia (2019), El Salvador (2021) y China (2021).',
+    answer:
+      'La visión de la OMS y de la comunidad mundial interesada en esta enfermedad es un mundo sin paludismo. Esta visión se logrará progresivamente a medida que los países eliminen el paludismo de sus territorios y apliquen medidas eficaces para prevenir el restablecimiento de la transmisión.Los países donde el paludismo es endémico se encuentran en diferentes fases del camino que lleva a la eliminación. La tasa de progreso depende de la solidez del sistema nacional de salud, el nivel de inversión en estrategias de eliminación del paludismo y otros factores, incluidos los determinantes biológicos, el medio ambiente y las realidades sociales, demográficas, políticas y económicas de cada país en particular.En los últimos dos decenios se han logrado avances significativos hacia la eliminación del paludismo. Según el último Informe mundial sobre el paludismo, 26 países registraron menos de 100 casos de la enfermedad en 2020, frente a 6 países en 2000.Los países donde durante por lo menos tres años consecutivos no se ha registrado ningún caso autóctono de paludismo (un caso contraído localmente sin datos de importación de otro país endémico) pueden solicitar a la OMS la certificación de la eliminación del paludismo. Once países han recibido la certificación desde el año 2000, siendo China y El Salvador los que la han obtenido más recientemente. Desde 2015, nueve países han recibido la certificación del Director General de la OMS de que están exentos de paludismo, a saber, Maldivas (2015), Sri Lanka (2016), Kirguistán (2016), el Paraguay (2018), Uzbekistán (2018), la Argentina (2019), Argelia (2019), El Salvador (2021) y China (2021).',
   },
 ];
