4aa03bbb9dd6965869d80148288a2adec30a8c8b
Florencia <florenciassoledadsosa@gmail.com>
2022-11-15 21:14:02 -0300

    firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { array } from './questions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'test-reading-time';
+export class AppComponent implements OnInit {
+ @Input() answers!: string;
+  // public answers!: string
+  public time!: Number;
+  public bdc = array;
+  private wpm = 225;
+
+  ngOnInit(): void {
+    this.readingTime();
+    console.log(this.answers);
+  }
+
+  public readingTime() {
+    // const words = this.answers.trim().split(/\s+/).length;
+    // this.time = Math.ceil(words / this.wpm);
+  }
 }
